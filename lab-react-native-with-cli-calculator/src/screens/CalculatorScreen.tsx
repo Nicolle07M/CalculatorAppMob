0d8fea2ea4efae269426aba09444e7303ae48fc5
Nicolle07M <marqueznicolle421@gmail.com>
2024-03-13 18:29:24 -0500

    feat(ReactNative): Create all buttons - fix the separation of the buttons

--- lab-react-native-with-cli-calculator/src/screens/CalculatorScreen.tsx
+++ lab-react-native-with-cli-calculator/src/screens/CalculatorScreen.tsx
@@ -13,8 +13,35 @@ export const CalculatorScreen = () => {
             <View style={styles.row}>
                 <CalculatorButton label="AC" color={colors.lightGray}/>
                 <CalculatorButton label="+/-" color={colors.lightGray}/>
-                <CalculatorButton label="%" color={colors.lightGray}/>
+                <CalculatorButton label="Del" color={colors.lightGray}/>
                 <CalculatorButton label="÷" color={colors.orange}/>
+            </View>
+
+            <View style={styles.row}>
+                <CalculatorButton label="7" color={colors.lightGray}/>
+                <CalculatorButton label="8" color={colors.lightGray}/>
+                <CalculatorButton label="9" color={colors.lightGray}/>
+                <CalculatorButton label="*" color={colors.orange}/>
+            </View>
+
+            <View style={styles.row}>
+                <CalculatorButton label="4" color={colors.lightGray}/>
+                <CalculatorButton label="5" color={colors.lightGray}/>
+                <CalculatorButton label="6" color={colors.lightGray}/>
+                <CalculatorButton label="-" color={colors.orange}/>
+            </View>
+
+            <View style={styles.row}>
+                <CalculatorButton label="1" color={colors.lightGray}/>
+                <CalculatorButton label="2" color={colors.lightGray}/>
+                <CalculatorButton label="3" color={colors.lightGray}/>
+                <CalculatorButton label="+" color={colors.orange}/>
+            </View>
+
+            <View style={styles.row}>
+                <CalculatorButton label="0" color={colors.lightGray} doubleSize/>
+                <CalculatorButton label="." color={colors.lightGray}/>
+                <CalculatorButton label="=" color={colors.orange}/>
             </View>
         </View>
     );
